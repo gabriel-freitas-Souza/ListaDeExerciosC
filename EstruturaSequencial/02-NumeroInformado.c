447c097dad2f40df097909057d4b3024b0b08761
gabriel-freitas-Souza <freitassouza26@gmail.com>
2021-03-29 09:02:07 -0300

    Create 02-NumeroInformado.c

---
+++ EstruturaSequencial/02-NumeroInformado.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int number = 0;
+    printf("Digite um nume \n -->");
+    scanf("%d", &number);
+    printf("\nO numero informado e %d \n",number);
+    return 0;
+}
